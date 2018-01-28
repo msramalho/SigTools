d4f4b885b26cd1137925e2705f073fccf4e0cbf2
msramalho <up201403027@fe.up.pt>
2018-01-28 00:05:29 +0000

    robust

--- extractors/timetable.js
+++ extractors/timetable.js
@@ -7,8 +7,11 @@ class ClassesTimetable {
 
     attachIfPossible() {
         if (this.table) {
+            console.log("found");
             let saveBtn = $('<a id="calendarBtn" title="Save this To your Calendar">📆</a>');
-            $("table.horario-semanas").before(saveBtn);
+            let timespan = $("table.horario-semanas");
+            if (timespan.length > 0) timespan.before(saveBtn);
+            else this.table.before(saveBtn);
             saveBtn.click((e) => {
                 this.timetable = this.timetable.events == undefined ? this.getEvents() : this.timetable;
                 handleEvents(this, this.timetable.from, this.timetable.to, this.timetable.events);
@@ -19,8 +22,21 @@ class ClassesTimetable {
     getEvents() {
         let table = this.table.parsetable(false, true);
         let lifetime = $(".bloco-select a").attr("href");
-        let lifetimeFrom = textToDate(lifetime.match(/p_semana_inicio=(\d*)/)[1]);
-        let lifetimeTo = textToDate(lifetime.match(/p_semana_fim=(\d*)/)[1]);
+        let lifetimeFrom, lifetimeTo;
+        if (lifetime) {
+            lifetimeFrom = textToDate(lifetime.match(/p_semana_inicio=(\d*)/)[1]);
+            lifetimeTo = textToDate(lifetime.match(/p_semana_fim=(\d*)/)[1]);
+        } else {
+            $("h3").each((index, h3) => {
+                if ($(h3).text().includes("Semanas de ")) {
+                    console.log($(h3).text().replace(" ", ""));
+                    let parts = $(h3).text().replace(" ", "").match(/(\d+-\d+-\d+)/g);
+                    console.log(parts);
+                    lifetimeFrom = textToDate(parts[0].replace("-",""));
+                    lifetimeTo = textToDate(parts[1].replace("-",""));
+                }
+            });
+        }
         return {
             from: lifetimeFrom,
             to: lifetimeTo,
