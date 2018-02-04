b220865fba6b2c1829f21aa8102b4991db712e88
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-04 19:18:26 +0000

    Finished class
    Added JS6 features, added some jQuery to keep consistency with the other code, changed from <input> to <textarea> on some elements, fixed some stuff

--- js/scripts/options.js
+++ js/scripts/options.js
@@ -1,36 +1,64 @@
 "use strict";
 //https://developer.chrome.com/extensions/content_scripts#run_at
 
-function loadFormats() {
+class stringFormats {
+    constructor() {
 
-    chrome.storage.local.get("moodle_title", function(obj) {
-        console.log(obj);
-        if(obj != undefined) {
-            document.getElementById("moodle_title").value = obj.moodle_title;
-        }
-    });
+        chrome.storage.local.get(null, (obj) => {
+            console.log(obj);
+            
+            this.moodle_title = (obj.moodle_title === undefined ? "%name% (%type%)" : obj.moodle_title);
+            this.moodle_desc = (obj.moodle_desc === undefined ? "%name% (%type%)\nlink:%link%" : obj.moodle_desc);
 
-    chrome.storage.local.get("moodle_desc", function(obj) {
-        console.log(obj);
-        if(obj != undefined) {
-            document.getElementById("moodle_desc").value = obj.moodle_desc;
+            $("#moodle_title").val(this.moodle_title);
+            $("#moodle_desc").val(this.moodle_desc);
+        });
+    }
+
+    saveFormats() {   
+        console.log(this.moodle_desc);
+        var settings = {
+            moodle_desc: this.moodle_desc,
+            moodle_title: this.moodle_title
         }
-    });
-    
-    
+        chrome.storage.local.set(settings);
+    }
+
+    //
+    // Getters
+    //
+    get moodleTitle() {
+        return this.moodle_title;
+    }
+
+    get moodleDescription() {
+        return this.moodle_desc;
+    }
+
+    //
+    // Setters
+    //
+    set moodleTitle(title) {
+        this.moodle_title = title;
+    }
+
+    set moodleDescription(desc) {
+        this.moodle_desc = desc;
+    }
 }
 
-function saveFormats() {
-    console.log("wuuuuuuut");
-    var moodle_desc = document.getElementById("moodle_desc").value;
-    var moodle_title = document.getElementById("moodle_title").value;
+var options = new stringFormats();
+// add onclick event for 'Save' button
+$("#btn_save").click(function() {
+    // Load text from HTML elements
+    var m_title = $("#moodle_title").val();
+    var m_desc = $("#moodle_desc").val();
 
-    chrome.storage.local.set({'moodle_desc': moodle_desc});
-    chrome.storage.local.set({'moodle_title': moodle_title});
-}
+    // Update class fields
+    options.moodleTitle = m_title;
+    options.moodleDescription = m_desc;
+    console.log(options);
 
-// add onclick event for button
-document.getElementById("btn_save").addEventListener("onclick", saveFormats);
-
-// load formats
-loadFormats();
+    // Update chrome.storage
+    options.saveFormats();
+});
