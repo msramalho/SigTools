612b786f9d0d53d51f1fd215f99682d96f8722df
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-07 20:00:46 +0000

    Functions to manage settings

---
+++ js/scripts/settings.js
@@ -0,0 +1,39 @@
+function asyncGetMoodleTitle() {
+    return new Promise(
+        function (resolve, reject) {
+            chrome.storage.local.get(null, (obj) => {                
+                resolve(obj.moodle_title === undefined ? "%name% (%type%)" : obj.moodle_title);
+            });
+        }
+    )
+}
+
+function asyncGetMoodleDescription() {
+    return new Promise(
+        function (resolve, reject) {
+            chrome.storage.local.get(null, (obj) => {                
+                resolve(obj.moodle_desc === undefined ? "%name% (%type%)" : obj.moodle_desc);
+            });
+        }
+    )
+}
+
+function asyncGetExamsTitle() {
+    return new Promise(
+        function (resolve, reject) {
+            chrome.storage.local.get(null, (obj) => {                
+                resolve(obj.exam_title === undefined ? "%name% (%type%)" : obj.exam_title);
+            });
+        }
+    )
+}
+
+function asyncGetExamsDescription() {
+    return new Promise(
+        function (resolve, reject) {
+            chrome.storage.local.get(null, (obj) => {                
+                resolve(obj.exam_desc === undefined ? "%name% (%type%)" : obj.exam_desc);
+            });
+        }
+    )
+}
