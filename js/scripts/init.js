82ee841e0a9fc0318dec597fa4ad45ca64c28e20
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-09 11:04:12 +0000

    Enhancements to only set default settings if the extension storage is empty.

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -1,11 +1,15 @@
 // set some initial values for formats
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.local.set({
-        "moodle_title": "${event.name} (${event.type})",
-        "moodle_desc": "Link: ${event.url}",
-        "class_title": "",
-        "class_desc": "",
-        "exam_title":"",
-        "exam_desc": ""
+    chrome.storage.local.get(null, (obj) => {                
+        if(Object(obj).keys.length == 0) { // set default values
+            chrome.storage.local.set({
+                "moodle_title": "${event.name} (${event.type})",
+                "moodle_desc": "Link: ${event.url}",
+                "class_title": "",
+                "class_desc": "",
+                "exam_title":"",
+                "exam_desc": ""
+            });
+        } 
     });
 })
