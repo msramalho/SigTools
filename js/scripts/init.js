1ddb1d450fff89f620670c0e30e7c69e075f5df4
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-09 10:56:32 +0000

    Fixed bug

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -1,8 +1,8 @@
 // set some initial values for formats
-chrome.runtime.onInstalled(function () {
+chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.local.set({
-        "moodle_title": "",
-        "moodle_desc": "",
+        "moodle_title": "${event.name} (${event.type})",
+        "moodle_desc": "Link: ${event.url}",
         "class_title": "",
         "class_desc": "",
         "exam_title":"",
