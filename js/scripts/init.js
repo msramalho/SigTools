9631adac2f9476d7c83f46890aec8c644afb75d6
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-09 09:32:30 +0000

    Adding new script to run after instalation

---
+++ js/scripts/init.js
@@ -0,0 +1,9 @@
+// set some initial values for formats
+chrome.storage.local.set({
+    "moodle_title": "",
+    "moodle_desc": "",
+    "class_title": "",
+    "class_desc": "",
+    "exam_title":"",
+    "exam_desc": ""
+})
