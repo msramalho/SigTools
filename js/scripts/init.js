30e44cd55ba2a9efd6c48b435b7c1d83a9379b40
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-09 09:40:50 +0000

    Updated manifest to run script on background; registered event on extension installation

--- js/scripts/init.js
+++ js/scripts/init.js
@@ -1,9 +1,11 @@
 // set some initial values for formats
-chrome.storage.local.set({
-    "moodle_title": "",
-    "moodle_desc": "",
-    "class_title": "",
-    "class_desc": "",
-    "exam_title":"",
-    "exam_desc": ""
+chrome.runtime.onInstalled(function () {
+    chrome.storage.local.set({
+        "moodle_title": "",
+        "moodle_desc": "",
+        "class_title": "",
+        "class_desc": "",
+        "exam_title":"",
+        "exam_desc": ""
+    });
 })
