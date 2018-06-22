c547e25483a9fe5942113a78957e722c1a8856a1
msramalho <up201403027@fe.up.pt>
2018-06-22 16:47:35 +0100

    updated extractor

--- js/extractors/extractor.js
+++ js/extractors/extractor.js
@@ -3,10 +3,34 @@
  * The base extractor that works as an interface for the other interfaces
  */
 class Extractor {
-    structure = {}
 
-    constructor() {}
+    /**
+     * Initialize variables: this.structure object, init, and attachIfPossible
+     */
+    constructor() {
+        this.structure = this.structure()
+    }
 
+    /**
+     * Must return an object that describes the class: name, description, parameters in its events, variables that go to the storage
+     */
+    structure() {
+        throw "Must implement structure method on children of Extractor"
+    }
+
+    /**
+     * function to be called after init, where all the initial HTML changes are made to the pages
+     */
+    attachIfPossible() {
+        throw "Must implement attachIfPossible method on children of Extractor"
+    }
+
+    /**
+     * simple wrapper that waits for init and then calls attachIfPossible, should be called in constructor of implementing classes
+     */
+    ready() {
+        this.init().then(this.attachIfPossible())
+    }
     /**
      * Loads the structure.storage values into memory or uses the defaults if not set
      */
@@ -14,11 +38,15 @@ class Extractor {
         let that = this;
         return new Promise(
             function(resolve, reject) {
-                chrome.storage.local.get(structure.name, (obj) => {
-                    structure.storage.forEach(option => {
-                        // if this option is not set yet, then use the default, else use stored value
-                        if (obj[option.name] == undefined) that[option.name] = option.default;
-                        else that[option.name] = obj[option.name];
+                chrome.storage.local.get(that.structure.extractor, (obj) => {
+                    obj=obj[that.structure.extractor]; // remove wrapper so that values are directly accessible
+                    getProperties(that.structure.storage).forEach(prop => {
+                        that.structure.storage[prop].forEach(option => {
+                            // if this option is not set yet, then use the default, else use stored value
+                            if (obj[option.name] == undefined) that[option.name] = option.default;
+                            else that[option.name] = obj[option.name];
+                            option.value = that[option.name]; // save the value in the structure
+                        });
                     });
                     resolve();
                 });
@@ -42,3 +70,5 @@ class Extractor {
         return parseStrFormat(event, this.description, this.isHTML);
     }
 }
+
+let EXTRACTORS = []; //each new extractor shall add an instance of itself to this variable
