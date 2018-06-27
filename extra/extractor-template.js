43d64de3e2601e005ec54d828ef2e16ae4cd0222
msramalho <up201403027@fe.up.pt>
2018-06-27 23:41:45 +0100

    extractor template

---
+++ extra/extractor-template.js
@@ -0,0 +1,60 @@
+"use strict";
+
+class TemplateExtractor extends Extractor {
+
+    constructor() {
+        super();
+        // INITIAL OPERATIONS
+        this.ready();
+    }
+
+    structure() {
+        return {
+            extractor: "the name of the extractor", // must be unique among extractors
+            description: "a simple description of what it does",
+            parameters: [{//a list of the parameters that can be used ny users
+                    name: "name of the parameter",
+                    description: "either describe or exemplify"
+                }
+                //... other parameters
+            ],
+            storage: { // the variables to save for this extractor (in the local storage)
+                text: [ //variables that should be displayed and edited in <input type="text">
+                    {
+                        name: "the name of the variable, eg: title",
+                        default: "The default value, eg: [${acronym}] - ${room.name}"
+                    }
+                ],
+                textarea: [ //variables that should be displayed and edited in <textarea></textarea>
+                    {
+                        name: "description",
+                        default: "another description - can have <strong>HTML</strong> inside"
+                    }
+                ],
+                boolean: [ //variables that should be displayed and edited in <input type="checkbox">
+                    {
+                        name: "isHTML",
+                        default: true
+                    }
+                ],
+                color: [{
+                    name: "colorValue",
+                    default: "#009684"
+                }]
+            }
+        }
+    }
+
+
+    attachIfPossible() {
+
+    }
+
+    convertToURI(original) {
+
+    }
+
+}
+
+// add an instance to the EXTRACTORS variable, and also trigger attachIfPossible due to constructor
+EXTRACTORS.push(new TemplateExtractor());
