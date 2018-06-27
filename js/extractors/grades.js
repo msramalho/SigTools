42581c08cc1140e41f69fc7bcd245e4e068f2f3f
msramalho <up201403027@fe.up.pt>
2018-06-27 16:14:06 +0100

    base histogram is ready

--- js/extractors/grades.js
+++ js/extractors/grades.js
@@ -16,10 +16,13 @@ class Grades extends Extractor {
             parameters: [],
             storage: {
                 text: [{
-                        name: "chart_min_width",
-                        default: "400px"
-                    }
-                ],
+                    name: "chart_min_width",
+                    default: "400px"
+                }],
+                color: [{
+                    name: "chart_color",
+                    default: "#009688"
+                }],
                 boolean: [{
                     name: "apply",
                     default: true
@@ -30,19 +33,25 @@ class Grades extends Extractor {
 
 
     attachIfPossible() {
-        //return if table not found
+        // return if table not found
         if (this.originalTable == undefined) return
 
-        let s = this.originalTable.html() + "";
+        // append the canvas
         this.originalTable.before(`<div class="gradeChartDiv" style="min-width: ${this.chart_min_width};"><canvas id="gradesChart"></canvas></div>`);
-        console.log(this.originalTable);
-        // this.originalTable.html( s);
-        // console.log(this.originalTable.html())
-        // let myBarChart = new Chart(ctx, {
-        //     type: 'bar',
-        //     data: this.getGrades(),
-        //     options: {}
-        // });
+
+        // generate the chart
+        new Chart($("#gradesChart").get(), {
+            type: 'bar',
+            data: {
+                labels: this.getLabels(),
+                datasets: [{
+                    label: "Grades",
+                    data: this.getAccumulatedGrades(),
+                    backgroundColor: "#009688"
+                }]
+            },
+            options: {}
+        });
     }
 
     /**
@@ -55,7 +64,7 @@ class Grades extends Extractor {
 
         this.grades = []
         //iterate rows, ignoring the header
-        this.table.find("tr:not(:first-child)").each((_, tr) => {
+        this.table.find("tr:has(>td)").each((_, tr) => {
             this.grades.push({
                 id: $(tr).find("td.n").html(),
                 name: $(tr).find("td:nth-child(2)").html(),
@@ -64,7 +73,30 @@ class Grades extends Extractor {
             })
         });
         return this.grades.map(o => o.grade)
+    }
 
+    /**
+     * Iterates the valid labels and counts the number of elements in the grades that match them
+     * @returns an array, aligned with the labels, of the counting operation
+     */
+    getAccumulatedGrades() {
+        let grades = this.getGrades(), res = []
+        this.getLabels().forEach(l => {
+            res.push([...grades].filter(g => g == l).length)
+        });
+        return res;
+    }
+
+    /**
+     * Returns an array with the labels RFC, RFE, ... that are present in the chart (to avoid empty columns)
+     * along with the numeric values from 10 to 20
+     */
+    getLabels() {
+        let mandatory = new Set(["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"])
+        let extra = new Set(["RFF", "RFC", "RFE", "RFR", "RA", "RD", "REC"])
+        let present = new Set(this.getGrades())
+        let intersect = new Set([...extra].filter(x => present.has(x))) // intersect extra with present
+        return Array.from(new Set([...intersect, ...mandatory])) // join intersection with mandatory
     }
 
     getEvents(index) {
