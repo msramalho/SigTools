a59b56c041cd9642564b62aa27e8e941b2f239e8
msramalho <up201403027@fe.up.pt>
2018-06-27 15:04:41 +0100

    grades format ready

--- js/extractors/grades.js
+++ js/extractors/grades.js
@@ -4,8 +4,8 @@ class Grades extends Extractor {
 
     constructor() {
         super();
-        this.table = $("table.dadossz").clone();
-
+        this.originalTable = $("table.dadossz");
+        this.table = this.originalTable.clone();
         this.ready();
     }
 
@@ -15,6 +15,11 @@ class Grades extends Extractor {
             description: "Produces a histogram and some statistics on the global grades page",
             parameters: [],
             storage: {
+                text: [{
+                        name: "chart_min_width",
+                        default: "400px"
+                    }
+                ],
                 boolean: [{
                     name: "apply",
                     default: true
@@ -25,26 +30,41 @@ class Grades extends Extractor {
 
 
     attachIfPossible() {
-        //iterate rows, ignoring the header
-        this.grades = []
-        this.table.find("tr:not(:first-child)").each((_, tr) => {
-            this.grades.push($(tr).find("td.n").html())
-        });
-        console.log(this.grades);
+        //return if table not found
+        if (this.originalTable == undefined) return
 
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: data,
-            options: {}
-        });
+        let s = this.originalTable.html() + "";
+        this.originalTable.before(`<div class="gradeChartDiv" style="min-width: ${this.chart_min_width};"><canvas id="gradesChart"></canvas></div>`);
+        console.log(this.originalTable);
+        // this.originalTable.html( s);
+        // console.log(this.originalTable.html())
+        // let myBarChart = new Chart(ctx, {
+        //     type: 'bar',
+        //     data: this.getGrades(),
+        //     options: {}
+        // });
     }
 
-    getGrades(){
-var myBarChart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: options
-});
+    /**
+     * Loads an object {id, name, course, grade} into this.grades
+     * @returns an array with the grade values (non-numeric included)
+     */
+    getGrades() {
+        //singleton implementation
+        if (this.grades != undefined) return this.grades.map(o => o.grade)
+
+        this.grades = []
+        //iterate rows, ignoring the header
+        this.table.find("tr:not(:first-child)").each((_, tr) => {
+            this.grades.push({
+                id: $(tr).find("td.n").html(),
+                name: $(tr).find("td:nth-child(2)").html(),
+                course: $(tr).find("td:nth-child(3)").html(),
+                grade: $(tr).find("td.n").html()
+            })
+        });
+        return this.grades.map(o => o.grade)
+
     }
 
     getEvents(index) {
