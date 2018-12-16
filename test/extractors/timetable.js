daaadf3713d38680c74764f4d5c89f96d63d21cd
msramalho <up201403027@fe.up.pt>
2018-12-16 01:48:07 +0000

    improved tests

--- test/extractors/timetable.js
+++ test/extractors/timetable.js
@@ -5,26 +5,41 @@ before(() => {
 })
 
 let t;
-describe('parse timetable', function() {
-    it('should create a valid timetable', function(done) {
+describe('Timetable extractor', function() {
+    it('should create a valid extractor', function(done) {
         t = new Timetable()
-        expect(t.structure.extractor).to.equal("timetable")
+        t.structure.should.contain.all.keys("description", "extractor", "parameters", "storage")
+        t.structure.extractor.should.equal("timetable")
         done()
     })
 
     it('should return list of events', function(done) {
-        // console.log(t.table.html());
-        t.events = t.getEvents()
-        console.log(t.events);
-        t.events.should.be.an('object')
+        t.timetable = t.getEvents()
+        t.timetable.should.be.an('object')
+        t.timetable.events.should.have.length(8)
+        t.timetable.events.forEach(event => {
+            event.should.contain.all.keys("acronym", "download", "from", "klass", "location", "name", "room", "teacher", "to", "type")
+            event.name.should.eql("Redes de Computadores")
+            event.download.should.be.true
+            event.acronym.should.eql("RCOM")
+        })
+        let first = t.timetable.events[0]
+        first.type.should.be.eql("TP")
+        first.teacher.name.should.be.eql("Manuel Pereira Ricardo")
+        first.teacher.acronym.should.be.eql("MPR")
+        first.location.should.be.eql("I320+I321")
+        first.room.name.should.be.eql("I320+I321")
+        first.room.url.should.include("hor_geral.composto_salas?p_c_sala=2594257")
+        first.klass.name.should.be.eql("3MIEIC07")
+        first.klass.url.should.include("hor_geral.turmas_view?pv_turma_id=207806&pv_ano_lectivo=2017&pv_periodos=1&pv_periodos=2&pv_periodos=4&pv_periodos=5&pv_periodos=8")
+        first.from.should.eql(new Date("2017-09-18 08:30"))
+        first.to.should.eql(new Date("2017-09-18 10:30"))
         done()
     }).timeout(5000)
 
     it('should return correct dates for RCOM', function(done) {
-        t.events.from.should.be.a('date')
-        t.events.from.should.eql(new Date("2017-09-16 24:00"))
-        t.events.to.should.be.a('date')
-        t.events.to.should.eql(new Date("2017-09-22 24:00"))
+        t.timetable.from.should.eql(new Date("2017-09-16 24:00"))
+        t.timetable.to.should.eql(new Date("2017-09-22 24:00"))
         done()
     })
 })
