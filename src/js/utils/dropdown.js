a87129c6f1831eded1f1df13b8b4c62e88f5552b
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-02-18 13:56:50 +0000

    refactor: Clean up the 'calendar' module

--- src/js/utils/dropdown.js
+++ src/js/utils/dropdown.js
@@ -1,3 +1,53 @@
+
+/**
+ * Returns an element object <a> for OneClick feature with a <img> child
+ * @param {string} class_atr_a The class for <a> element
+ * @param {string} class_atr_img The class for <img> child element
+ * @param {string} service 'google' || 'outlook'. This is used to set the correct title and icon automatically
+ * @param {string} url
+ * @param {boolean} html URL's with inline html or with plain text require different encodings
+ */
+function getAddToCalendarDOM(class_atr_a, class_atr_img, service, url, html, title) {
+    var a = document.createElement("a");
+    var img = document.createElement("img");
+
+    // set class
+    a.className = class_atr_a;
+    img.className = class_atr_img;
+
+    // set title and append an <img>
+    if (service == "google") {
+        a.setAttribute("title", "Add this single event to your Google Calendar in one click!");
+        img.setAttribute("alt", "google calendar icon");
+        img.setAttribute("src", `${chrome.extension.getURL("icons/gcalendar.png")}`);
+    } else if (service == "outlook") {
+        a.setAttribute("title", "Add this single event to your Outlook Calendar in one click!");
+        img.setAttribute("alt", "outlook calendar icon");
+        img.setAttribute("src", `${chrome.extension.getURL("icons/outlook.png")}`);
+    } else if (service == "office365") {
+        a.setAttribute("title", "Add this single event to your Office365 Calendar in one click!");
+        img.setAttribute("alt", "office 365 icon");
+        img.setAttribute("src", `${chrome.extension.getURL("icons/office365.png")}`);
+    } else if (service == "yahoo") {
+        a.setAttribute("title", "Add this single event to your Yahoo Calendar in one click!");
+        img.setAttribute("alt", "yahoo calendar icon");
+        img.setAttribute("src", `${chrome.extension.getURL("icons/yahoo.png")}`);
+    }
+    a.appendChild(img);
+
+    // add href attribute to automatically set the pointer/cursor
+    a.setAttribute("href", "#");
+    if (title != undefined) a.innerHTML += title
+
+    // add event listener
+    if (html)
+        a.setAttribute("onclick", `window.open(decodeURI('${encodeURI(url.replace(/'/g, "\""))}').replace(/\\s/g, "%20"));`);
+    else
+        a.setAttribute("onclick", `window.open('${url.replace(/\n/g, '%0A')}');`);
+
+    return a;
+}
+
 /**
  * Return a Jquery element that has a dropdown and the dropdown buttons for a single event
  * @param {Event} event
@@ -22,10 +72,10 @@ function getDropdown(event, context, repeat, style) {
 	<div class="${style.divClass}" style="${style.divStyle}">
 		<a class="${style.aClass}" target="${style.target}" title="Save this event to your Calendar"><img target="${style.target}" src="${chrome.extension.getURL("icons/calendar.svg")}"/></a>
         <div id="${style.target}" class="${style.dropdownClass}" style="${style.dropdownStyle}">
-        ${generateOneClickDOM("", "dropdownIcon", "google", google_url, context.isHTML, "Google").outerHTML}
-        ${generateOneClickDOM("", "dropdownIcon", "outlook", outlook_url, context.isHTML, "Outlook").outerHTML}
-        ${generateOneClickDOM("", "dropdownIcon", "office365", office365_url, context.isHTML, "Office 365").outerHTML}
-        ${generateOneClickDOM("", "dropdownIcon", "yahoo", yahoo_url, context.isHTML, "Yahoo").outerHTML}
+        ${getAddToCalendarDOM("", "dropdownIcon", "google", google_url, context.isHTML, "Google").outerHTML}
+        ${getAddToCalendarDOM("", "dropdownIcon", "outlook", outlook_url, context.isHTML, "Outlook").outerHTML}
+        ${getAddToCalendarDOM("", "dropdownIcon", "office365", office365_url, context.isHTML, "Office 365").outerHTML}
+        ${getAddToCalendarDOM("", "dropdownIcon", "yahoo", yahoo_url, context.isHTML, "Yahoo").outerHTML}
 		<a class="donwloadSingleIcs" data='${JSON.stringify(event)}' style="background-color:#e3e3e3" title="For other calendars, download the single .ics file" href="#"><img class="dropdownIcon" alt="apple calendar icon" src="${chrome.extension.getURL("icons/calendar.png")}">Others (.ics)</a>
 		</div>
 	</div>`);
