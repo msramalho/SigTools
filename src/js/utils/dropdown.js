7de278fcf2542a6686ea94cb7f0cab6b2ae25333
Fabio Gaspar <fabiodanielreis@hotmail.com>
2022-03-27 16:11:12 +0100

    refactor(dropdown): Simplify and support the new CalEvent class
    The new approach creates all the necessary DOM and adds event listeners
    automatically. It also supports the new CalEvent class to represent
    calendar events and generate the inline dropdowns from them

--- src/js/utils/dropdown.js
+++ src/js/utils/dropdown.js
@@ -47,6 +47,7 @@ function generateAnchorWithIcon(a, img, title, src){
 }
 
 /**
+ * @deprecated
  * Return a Jquery element that has a dropdown and the dropdown buttons for a single event
  * @param {Event} event
  * @param {Context} context the "this" value before (must be an extractor with getName, get Description and isHTML)
@@ -80,6 +81,7 @@ function getDropdown(event, context, repeat, style) {
 }
 
 /**
+ * @deprecated
  * unbind and rebind the listeners for the dropdown buttons
  */
 function setDropdownListeners(extractor, repeat) {
@@ -95,6 +97,7 @@ function setDropdownListeners(extractor, repeat) {
 }
 
 /**
+ * @deprecated
  * Toggle dropdown buttons
  */
 function toggleDropdown(btn) {
@@ -112,3 +115,97 @@ window.onclick = function (event) {
         })
     }
 }
+
+/**
+ * Creates the dropdown for adding singular events to a calendar platform.
+ * Supported platforms are:
+ * - Microsoft (Outlook and Office 365)
+ * - Google
+ * - Yahoo
+ * 
+ * The dropdown includes an option to download the ICS for the event. Clicking
+ * on a platform opens an URL in a new tab for composing a new event in the
+ * selected platform. The URL is generated with query parameters to pre-fill
+ * most of the fields with extracted information from Sigarra.
+ * 
+ * The generated DOM includes:
+ * - button to open the dropdown
+ * - the dropdown menu itself (hidden/closed by default)
+ * - open links in a new tab when dropdown items are clicked (e.g. Google
+ * Calendar)
+ * - behaviour to show/hide the dropdown items, as well as any other dropdowns
+ * so that only one dropdown is open at a time
+ * - automatically close all dropdowns when clicking outside the dropdown in
+ * the page
+ * 
+ * @param {CalEvent} event 
+ * @param {Object?} style 
+ * @param {String?} style.divClass Class for the dropdown's container div
+ * @param {String?} style.divStyle Inline styles for the dropdown's container
+ * div
+ * @param {String?} style.aClass Class for the 'add event' button that opens
+ * the dropdown
+ * @param {String?} style.dropdownClass Class for the dropdown menu
+ * @param {String?} style.dropdownStyle Inline styles for the dropdown menu
+ * @returns {HTMLElement} The dropdown container 
+ */
+function createEventDropdown(event, style) {
+    const urlGenerator = new CalendarUrlGenerator(event);
+    const googleUrl = urlGenerator.google();
+    const outlookUrl = urlGenerator.outlook();
+    const officeUrl = urlGenerator.office365();
+    const yahooUrl = urlGenerator.yahoo();
+
+    // prepare the styling for the dropdown
+    style = style || {};
+    style.divClass = style.divClass || "dropdown right";
+    style.divStyle = style.divStyle || "position:initial;";
+    style.aClass = style.aClass || "calendarBtn dropBtn";
+    style.dropdownClass = style.dropdownClass || "dropdown-content";
+    style.dropdownStyle = style.dropdownStyle || "";
+
+    const $dropdown = createElementFromString(`
+	<div class="sigDropdown ${style.divClass}" style="${style.divStyle}">
+		<a class="sigDropdown__btn ${style.aClass}" title="Save this event to your Calendar">
+            <img src="${chrome.extension.getURL("icons/calendar.svg")}"/>
+        </a>
+        <div class="sigDropdown__menu ${style.dropdownClass}" style="${style.dropdownStyle}">
+            ${getAddToCalendarDOM("", "dropdownIcon", "google", googleUrl, event.isHTML, "Google").outerHTML}
+            ${getAddToCalendarDOM("", "dropdownIcon", "outlook", outlookUrl, event.isHTML, "Outlook").outerHTML}
+            ${getAddToCalendarDOM("", "dropdownIcon", "office365", officeUrl, event.isHTML, "Office 365").outerHTML}
+            ${getAddToCalendarDOM("", "dropdownIcon", "yahoo", yahooUrl, event.isHTML, "Yahoo").outerHTML}
+            <a class="donwloadSingleIcs"
+                style="background-color:#e3e3e3"
+                title="For other calendars, download the single .ics file"
+                href="#">
+                <img class="dropdownIcon" alt="apple calendar icon" src="${chrome.extension.getURL(
+                    "icons/calendar.png"
+                )}">
+                Others (.ics)
+            </a>
+		</div>
+	</div>`);
+
+    // add click action for the ICS download button
+    $dropdown.querySelector(".donwloadSingleIcs").addEventListener("click", (e) => {
+        const cal = ics("sigtools"); //creat ics instance
+        cal.addEvent(event.title, event.description, event.location, event.start, event.end, event.recurRule);
+    });
+
+    // on clicking the open dropdown menu, close all other dropdowns, and then show this drop menu
+    $dropdown.querySelector(".sigDropdown__btn").addEventListener("click", (e) => {
+        for (const $el of document.querySelectorAll(".sigDropdown__menu")) $el.classList.remove("show");
+        $dropdown.querySelector(".sigDropdown__menu").classList.toggle("show");
+    });
+
+    return $dropdown;
+}
+
+window.addEventListener("click", (ev) => {
+    // if there is a click outside the dropdown, close all dropdowns
+    const $targetDrop = ev.target.closest('.sigDropdown');
+    if ($targetDrop === null) {
+        for (const $el of document.querySelectorAll('.sigDropdown__menu'))
+            $el.classList.remove("show");
+    }
+})
