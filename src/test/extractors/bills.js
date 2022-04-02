56699424ba6ff0d67a8e68bde873fe4530b79bd7
Fabio Gaspar <fabiodanielreis@hotmail.com>
2022-04-02 23:31:01 +0100

    test: Update unit tests for bill extractor

--- src/test/extractors/bills.js
+++ src/test/extractors/bills.js
@@ -1,42 +1,96 @@
-describe('Bill extractor', function() {
-    before(() => {
-        return new Promise((resolve) => {
-            updatejQueryContext("conta_corrente.html").then(resolve)
-        })
-    })
-    let e, b;
-    it('should create a valid extractor', function(done) {
-        e = new Bill()
-        e.structure.should.contain.all.keys("description", "extractor", "parameters", "storage")
-        e.structure.extractor.should.equal("bills")
-        // e.apply = true
-        // e.attachIfPossible()
-        done()
-    })
-    it('should find all the bills in the page', function(done) {
-        b = e._getBills()
-        b.should.have.length(2)
-        done()
-    })
-    it('should parse all the bills properly', function(done) {
-        b = b.map(e._parsePendingBill)
-        b.forEach(bill => {
-            bill.should.contain.all.keys("amount", "date", "description", "from", "to", "location")
-            bill.download.should.eql(false)
-        })
-        // b[0].amount.should.equal("120,00 €")
-        b[0].amount.should.have.length(8)
-		b[0].description.should.equal("Carta de Curso - Licenciatura - Mestrado Integrado em Engenharia Informática e Computação - Pedido de Certificado: Carta de Curso - Grau de Licenciado")
-        b[0].from.should.eql(new Date("2018-12-22 00:00"))
-        b[0].to.should.eql(new Date("2019-01-08 00:00"))
-        b[0].date.should.eql(new Date("2019-01-08 00:00"))
+describe("Bill extractor", function () {
+    describe("Normal page without ATM refs", function () {
+        before(function () {
+            return updatejQueryContext("bills/normal.html").then(() => {
+                this.bills = new Bills();
+            });
+        });
 
-        // b[1].amount.should.equal("5,00 €")
-        b[1].amount.should.have.length(6)
-        b[1].description.should.equal("Certidão de Aproveitamento Escolar - Mestrado Integrado em Engenharia Informática e Computação - Pedido de Certificado: Certidão de Aproveitamento Escolar")
-        b[1].from.should.eql(new Date("2018-12-22 00:00"))
-        b[1].to.should.eql(new Date("2019-01-08 00:00"))
-        b[1].date.should.eql(new Date("2019-01-08 00:00"))
-        done()
-    })
-})
+        it("should find all bills", function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const all = bills.parsePendingBills();
+            chai.assert.strictEqual(all.length, 5);
+            done();
+        });
+
+        it("should handle bills without deadlines", function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const all = bills.parsePendingBills();
+            chai.assert.strictEqual(all[4].deadline, null);
+            done();
+        });
+
+        it("should calculate total amount (include fees)", function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const all = bills.parsePendingBills();
+            // with fee
+            chai.assert.include(all[0].amount, "34,95");
+            // without fee
+            chai.assert.include(all[1].amount, "34,85");
+            done();
+        });
+
+        it("should parse correctly", function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const all = bills.parsePendingBills();
+
+            chai.assert.equal(
+                all[0].description,
+                "Propinas - Mestrado em Engenharia Informática e Computação - Prestação 7 2021/2022"
+            );
+            chai.assert.equal(all[0].deadline, "2022-03-31");
+            chai.assert.include(all[0].amount, "34,95");
+
+            chai.assert.equal(
+                all[1].description,
+                "Propinas - Mestrado em Engenharia Informática e Computação - Prestação 8 2021/2022"
+            );
+            chai.assert.equal(all[1].deadline, "2022-04-30");
+            chai.assert.include(all[1].amount, "34,85");
+
+            chai.assert.equal(
+                all[2].description,
+                "Carta de Curso - Licenciatura - Mestrado Integrado em Engenharia Informática e Computação - Pedido de Certificado: Carta de Curso - Grau de Licenciado"
+            );
+            chai.assert.equal(all[2].deadline, "2019-01-08");
+            chai.assert.include(all[2].amount, "120,00");
+
+            chai.assert.equal(
+                all[3].description,
+                "Certidão de Aproveitamento Escolar - Mestrado Integrado em Engenharia Informática e Computação - Pedido de Certificado: Certidão de Aproveitamento Escolar"
+            );
+            chai.assert.equal(all[3].deadline, "2019-01-08");
+            chai.assert.include(all[3].amount, "5,00");
+
+            chai.assert.equal(
+                all[4].description,
+                "Propinas - Mestrado em Engenharia Informática e Computação - Prestação 0 2022/2023"
+            );
+            chai.assert.equal(all[4].deadline, null);
+            chai.assert.include(all[4].amount, "34,85");
+            done();
+        });
+
+        it("should create CalendarEvents only if deadline exists", function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const events = bills.getEvents();
+            chai.assert.equal(events.length, 4);
+            done();
+        });
+
+        it('should create CalendarEvents as "All Day" events', function (done) {
+            /** @type {Bills} */
+            const bills = this.bills;
+            const events = bills.getEvents();
+            for (const e of events) {
+                chai.assert.isTrue(e._allDay);
+            }
+            done();
+        });
+    });
+});
