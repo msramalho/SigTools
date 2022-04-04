38c1422a8b7b1788b1a3b759467018022fa241f6
Fabio Gaspar <fabiodanielreis@hotmail.com>
2022-04-04 22:52:03 +0100

    feat(utils): Add function for deep object merging

--- src/js/utils/global.js
+++ src/js/utils/global.js
@@ -128,3 +128,32 @@ function createElementFromString(htmlString) {
     div.innerHTML = htmlString.trim();
     return div.firstElementChild;
 }
+
+/**
+ * Performs a deep merge of objects and returns new object. Does not modify
+ * objects (immutable) and merges arrays via concatenation.
+ *
+ * Shamelessly copied from {@link https://stackoverflow.com/a/48218209} :)
+ * @param {...object} objects - Objects to merge
+ * @returns {object} New object with merged key/values
+ */
+ function mergeDeep(...objects) {
+    const isObject = (obj) => obj && typeof obj === "object";
+
+    return objects.reduce((prev, obj) => {
+        Object.keys(obj).forEach((key) => {
+            const pVal = prev[key];
+            const oVal = obj[key];
+
+            if (Array.isArray(pVal) && Array.isArray(oVal)) {
+                prev[key] = pVal.concat(...oVal);
+            } else if (isObject(pVal) && isObject(oVal)) {
+                prev[key] = mergeDeep(pVal, oVal);
+            } else {
+                prev[key] = oVal;
+            }
+        });
+
+        return prev;
+    }, {});
+}
